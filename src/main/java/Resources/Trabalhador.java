09577e828cee11d577df8411e0f71b0fee80b3c3
jtacopt <33877612+jtacopt@users.noreply.github.com>
2018-02-24 18:56:32 +0000

    Commit new shit

---
+++ src/main/java/Resources/Trabalhador.java
@@ -0,0 +1,38 @@
+package Resources;
+
+public class Trabalhador {
+
+	private String Nome;
+	private String UnidadePai;
+	private String DescriçãoFuncao;
+	
+	public Trabalhador() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDescriçãoFuncao() {
+		return DescriçãoFuncao;
+	}
+
+	public void setDescriçãoFuncao(String descriçãoFuncao) {
+		DescriçãoFuncao = descriçãoFuncao;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getUnidadePai() {
+		return UnidadePai;
+	}
+
+	public void setUnidadePai(String unidadePai) {
+		UnidadePai = unidadePai;
+	}
+	
+	
+}
